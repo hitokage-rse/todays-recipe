510614256009d8fba9154f4b5d7c0ddbc209e317
hitokage-rse <zhongcunyuji@gmail.com>
2022-12-02 23:54:54 +0900

    xcworkspace追加・spmpackage追加・tcaライブラリ追加

---
+++ todays-recipe/todays_recipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  todays_recipeApp.swift
+//  todays-recipe
+//
+//  Created by kagehito on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct todays_recipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ todays-recipeUITests/todays_recipeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  todays_recipeUITestsLaunchTests.swift
+//  todays-recipeUITests
+//
+//  Created by kagehito on 2022/12/02.
+//
+
+import XCTest
+
+class todays_recipeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
